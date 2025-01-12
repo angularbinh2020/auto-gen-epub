175ecfcf81326800d1f721d48983bdbc6e8ef44c
doducbinh <61402063+angularbinh2020@users.noreply.github.com>
2025-01-12 11:18:21 +0700

    update tool

--- gen-ebook.mjs
+++ gen-ebook.mjs
@@ -15,23 +15,29 @@ export const genEbook = async ({ maxChapter, startChapterUrl }) => {
     new Date(),
     "hh mm DD MM"
   )}`;
-  const filePath = resolve(__dirname, `./${fileName}.epub`);
-//   const azw3FilePath = resolve(__dirname, `./${fileName}.azw3`);
+  const filePath = resolve(__dirname, "books", `./${fileName}.epub`);
+  //   const azw3FilePath = resolve(__dirname, `./${fileName}.azw3`);
   console.log(filePath);
 
   const epub = new EPub(
     {
       title: bookTitle,
       description: `Start chapter: ${startChapterUrl} ; End chapter: ${endChapterUrl}`,
-      content: [...chapters,{title:"Phụ lục",data:`<div>Start chapter: ${startChapterUrl} ; End chapter: ${endChapterUrl}</div>`}],
+      content: [
+        ...chapters,
+        {
+          title: "Phụ lục",
+          data: `<div>Start chapter: ${startChapterUrl} ; End chapter: ${endChapterUrl}</div>`,
+        },
+      ],
     },
     filePath
   );
   await epub.render();
   console.log(">>> Export epub done");
-//   await convertToAzw3({
-//     input: filePath,
-//     output: azw3FilePath,
-//   });
-//   console.log(">>> Export azw3 done");
+  //   await convertToAzw3({
+  //     input: filePath,
+  //     output: azw3FilePath,
+  //   });
+  //   console.log(">>> Export azw3 done");
 };
--- server.mjs
+++ server.mjs
@@ -2,18 +2,24 @@
 import { genEbook } from "./gen-ebook.mjs";
 const MAX_CHAPTER = 50;
 const ebookUrls = [
-  // "https://truyenwikidich.net/truyen/tu-vong-tro-choi-tro-choi-nay-noi-nhu-th/chuong-1-dong-hoc-se-ZfJKncQsRF3PEYTY",
-  "https://truyenwikidich.net/truyen/xuyen-nhanh-co-chap-dai-lao-lai-bang-nan/chuong-1-mat-the-the-than-phao-hoi-1-ZdOcScQsREKP9q4J",
-  "https://truyenwikidich.net/truyen/xuyen-nhanh-vo-tinh-vai-ac-cam-tham-tinh/chuong-1-khe-uoc-thanh-hon-chi-truy-the--ZdizRcQsREKQ96J3",
-  "https://truyenwikidich.net/truyen/tu-thi-ngu-hung-trach-bat-dau/chuong-1-ky-hop-dong-ZfJKmsQsRF3QEXpv",
-  "https://truyenwikidich.net/truyen/nu-phu-nghi-thoang-roi-xuyen-nhanh/chuong-1-vo-truoc-1-XXTfhMQsRBlbz506",
-  "https://truyenwikidich.net/truyen/truyen-tranh-nu-n-hao-sau-khi-thuc-tinh/phan-1-ZgGFcMQsRGIvU8hF",
-  "https://truyenwikidich.net/truyen/su-pho-khong-duoc-roi-dai-su-huynh-cung-/chuong-1-tu-chuong-ZkxLs8QsRHkOt6tW",
-  "https://truyenwikidich.net/truyen/ta-co-doc-dao-tay-trang-ky-xao-xuyen-nha/phan-1-Zk3sCcQsRAahMOBP",
-  "https://truyenwikidich.net/truyen/trong-sinh-o-bi-giet-the-chung-dao-phia-/chuong-1-nang-trong-sinh-ZlH9dcQsRHkOuJNl",
-  "https://truyenwikidich.net/truyen/nghe-duoc-thien-dao-tieng-long-sau-tu-ti/trang-1-ZbNKJsQsREKQ766w",
-  "https://truyenwikidich.net/truyen/cuu-thien-tue-la-nu-nhi-than-tho-bao-tan/chuong-1-nu-gia-nam-trang-gia-thai-giam-ZlNmp8QsRHkOuMUj",
-  "https://truyenwikidich.net/truyen/xuyen-nhanh-ky-chu-lai-dien-khung/chuong-1-troi-dinh-1-ZlQ7g8QsRHkOuNtO",
+  "https://truyenwikidich.net/truyen/xuyen-thu-chi-phao-hoi-nguyen-phoi-bai-l/chuong-1-ZldCwMQsRHkOuVnd",
+  "https://truyenwikidich.net/truyen/cai-nay-kich-ban-sat-tuyet-doi-co-van-de/chuong-1-nay-co-thoa-thoa-luan-hoi-gia-h-YWIz_cQsRAO61WRt",
+  "https://truyenwikidich.net/truyen/xuyen-nhanh-ky-chu-nang-phao-hoi-than-la/chuong-1-le-tang-ZllgDcQsRHkPuhX6",
+  "https://truyenwikidich.net/truyen/doan-menh-lao-cong-di-san-nhieu-hao-mon-/chuong-1-ke-thua-chuc-ty-di-san-ZlzD18QsRHkPurkH",
+  "https://truyenwikidich.net/truyen/thieu-gia-co-benh-lam-sao-bay-gio-bai-la/chuong-1-xuyen-thu-Zly6RcQsRHkPurd0",
+  "https://truyenwikidich.net/truyen/quan-mon-quy-su/phan-1-ZlwJfcQsRHkOuf82",
+  "https://truyenwikidich.net/truyen/phat-he-thu-nu-sinh-ton-so-tay/phan-1-ZmIHCMQsRHkPu3Pk",
+  "https://truyenwikidich.net/truyen/pokemon-the-gioi-tang-duoi-chot-huan-luy/chuong-1-khong-giong-nhau-pokemon-the-gi-YRhvb8QsRCSX2qe_",
+  "https://truyenwikidich.net/truyen/cuu-vot-ca-ca-dai-tac-chien-xuyen-nhanh/phan-1-ZmKBz8QsRDYzzlE5",
+  "https://truyenwikidich.net/truyen/lam-cai-gi-nu-xung-cung-muon-mang-song/chuong-1-sao-tich-ta-menh-lien-khong-pha-ZmdXAcQsRHkOu%7EkH",
+  "https://truyenwikidich.net/truyen/duong-vai-ac-troi-dinh-nu-chu-he-thong/lieu-am-hoa-minh-1-Y%7E%7ELx8QsRAadJSsC",
+  "https://truyenwikidich.net/truyen/mau-xuyen-thanh-vai-ac-dai-lao-nu-nhi-sa/chuong-1-bi-tu-hon-thai-tu-phi-1-Zmh0usQsRHkPvLgC",
+  "https://truyenwikidich.net/truyen/xuyen-thu-tro-thanh-duong-nhai-con-dai-s/phan-1-Zn0BdMQsRAwwB79G",
+  "https://truyenwikidich.net/truyen/hau-cung-tru-ta-ngoai-deu-co-ban-tay-van/phan-1-ZoJutcQsRCtwB8G0",
+  "https://truyenwikidich.net/truyen/xuyen-qua-sau-lam-an-choi-trac-tang/phan-1-ZhqCccQsRANl8AV1",
+  "https://truyenwikidich.net/truyen/xuyen-qua-chi-nguoi-toi-tram-muon-thoai-/1-chuong-1-Zj2TdcQsRGJ7Ytzi",
+  "https://truyenwikidich.net/truyen/ac-doc-nu-xung-thay-lan-dan-sau-sat-dien/chuong-1-ta-thanh-ac-doc-nu-xung-ZkJODMQsRHkOtk75",
+  "https://truyenwikidich.net/truyen/ta-noi-dan-giai-tram-doc-kia-cung-khong-/chuong-1-nguoi-nhu-the-nao-con-khong-cao-Zkol5MQsRHkOtzmG",
 ];
 let ebookIndex = 0;
 while (ebookUrls[ebookIndex]) {
